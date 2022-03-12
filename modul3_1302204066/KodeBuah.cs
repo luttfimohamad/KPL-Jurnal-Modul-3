55833145f44020e4fc544d953532c6b016351683
luttfimohamad <luttfimohamad@gmail.com>
2022-03-12 16:12:48 +0700

    menambahkan class KodeBuah

---
+++ modul3_1302204066/KodeBuah.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul3_1302204066
+{
+    internal class KodeBuah
+    {
+        public string getKodeBuah(string Buah)
+        {
+            if (Buah == "Apel")
+            {
+                return "A00";
+            }
+            else if (Buah == "Aprikot")
+            {
+                return "B00";
+            }
+            else if (Buah == "Alpukat")
+            {
+                return "C00";
+            }
+            else if (Buah == "Pisang")
+            {
+                return "D00";
+            }
+            else if (Buah == "Paprika")
+            {
+                return "E00";
+            }
+            else if (Buah == "Blackberry")
+            {
+                return "F00";
+            }
+            else if (Buah == "Ceri")
+            {
+                return "H00";
+            }
+            else if (Buah == "Kelapa")
+            {
+                return "I00";
+            }
+            else if (Buah == "Jagung")
+            {
+                return "J00";
+            }
+            else if (Buah == "Kurma")
+            {
+                return "K00";
+            }
+            else if (Buah == "Durian")
+            {
+                return "L00";
+            }
+            else if (Buah == "Anggur")
+            {
+                return "M00";
+            }
+            else if (Buah == "Melon")
+            {
+                return "N00";
+            }
+            else if (Buah == "Semangka")
+            {
+                return "O00";
+            }
+            else
+            {
+                return "Tidak tersedia";
+            }
+        }
+    }
+}
--- modul3_1302204066/Program.cs
+++ modul3_1302204066/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Nama        : Mohamad Lutfi
+NIM         : 1302204066
+Kelas       : SE-44-02
+ */
+
+using System;
+
+namespace modul3_1302204066
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------ MENCARI KODE BUAH ------------");
+            Console.WriteLine("Daftar buah yang terssedia: ");
+            Console.WriteLine("1. Apel          " + " 8. Kelapa");
+            Console.WriteLine("2. Aprikot       " + " 9. Jagung");
+            Console.WriteLine("3. Alpukat       " + " 10. Kurma");
+            Console.WriteLine("4. Pisang        " + " 11. Durian");
+            Console.WriteLine("5. Paprika       " + " 12. Anggur");
+            Console.WriteLine("6. Blackberry    " + " 13. Melon");
+            Console.WriteLine("7. Ceri          " + " 14. Semangka");
+
+            Console.Write("Masukkan nama buah: ");
+            string masukan = Console.ReadLine();
+            if (masukan == "")
+            {
+                Console.Write("*Anda tidak memasukkan nama buah" + "\n");
+            }
+
+            KodeBuah kodeBuah = new KodeBuah();
+            Console.WriteLine("Kode Buah "+ masukan + ": " + kodeBuah.getKodeBuah(masukan) + "\n");
+
+        }
+    }
+}
